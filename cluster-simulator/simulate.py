52be80635488d0e92982b863c8071faa1683a9db
Bowen Hou <bowen.hou@autodesk.com>
2024-01-23 18:20:20 -0500

    update simulation

--- cluster-simulator/simulate.py
+++ cluster-simulator/simulate.py
@@ -1,47 +1,116 @@
 # %%
-from utils import load_workload, load_config
+from utils import load_workload, load_config, get_min_nodes_lst, get_num_task_per_node
 import math
 
 data = load_workload('workload3')
 config = load_config()
 
-
+# %%
+# This function is used to simulate the cluster scaling process
 def scale_workload(data, config):
-    # This function is still work in progress
-    result = {}
-    max_time = max([task.time for task in data])
+    result = []
+
+    # get the maximum time needed to complete the entire workload
+    max_time = [task.arrival_time + task.deadline for task in data]
+    
     processed_task = 0
     rejected_task = 0
 
+    # Initialize the cluster, where each node is empty for every second of the timeline
+    default_cluster = [0] * 500
+    cluster_state = [default_cluster for _ in range(config['Initial_Node_Count'])]
+
+    # load the constant time to complete a task
+    execution_time = config['Time_To_Complete_Task']
+
+    # Start the simulation, where each iteration represents one second
     for i in range(max_time):
-        acceptace_rate = int((processed_task / processed_task + rejected_task) * 100)
-        curr_task = []
+        # Calculate the acceptance rate
+        if processed_task + rejected_task == 0:
+            acceptace_rate = 0
+        else:
+            acceptace_rate = int((processed_task / processed_task + rejected_task) * 100)
+        
+        task_queue = []
         # check if it is time to bring in new task
-        if i == data[0].arr_tme:
+        if i == data[0].arrival_time:
             print(f'Processing task: {data[0]} at time {i}')
-            curr_task.append(data.pop(0))
-            parallization = get_num_task_per_node(curr_task, config)
+            task_queue.append(data.pop(0))
+            parallization = get_num_task_per_node(task_queue, config)
     pass
         
+def task_can_be_processed(task, cluster_state, parallelization, execution_time):
+    # check if there is enough nodes to process the task
+    available_space = sum([node[task.arrival_time:task.deadline].count(0) for node in cluster_state])
+    required_space = math.ceil(task.num_tasks/parallelization) * execution_time
+    return available_space >= required_space
 
-def get_min_nodes_lst(data, config):
-    lst = []
-    for workload in data:
-        lst.append(calc_min_nodes(workload, config))
-    return lst
+# this is the task schedule function, which marks occupied space in the cluster as 1
+def process_task(task, cluster_state, parallelization, execution_time):
+    required_space = math.ceil((task.num_tasks/parallelization) * execution_time)
+    print(f'The required space is: {required_space} slots')
+    
+    # Use round robin to schedule the task
+    node_idx = 0
+    num_nodes = len(cluster_state)
+    while required_space > 0:
+        print(f'Processing node {node_idx}', 'the current node state is: ', cluster_state[node_idx])
+        node = cluster_state[node_idx].copy()
+        for i in range(task.arrival_time, task.arrival_time + task.deadline):
+            if node[i] == 0:
+                node[i] = 1
+                required_space -= 1
+                if required_space == 0:
+                    break
+        cluster_state[node_idx] = node
+        node_idx = (node_idx + 1) % num_nodes
+        if required_space == 0:
+            return cluster_state
+    return cluster_state
 
-# find the minimum number of nodes needed to complete the workload
-def calc_min_nodes(task, config):
-    # load initial variables
-    node_count = 1
-    parallelization = get_num_task_per_node(task, config)
-    execution_time = config['Time_To_Complete_Task']
-    while (task.num_tasks * execution_time)/(node_count * parallelization) > task.deadline:
-        node_count += 1
-    return node_count
+# %%
+'''
 
-def get_num_task_per_node(task, config):
-    cpu_cap = config['Max_CPU_Utilization']/task.cpu
-    mem_cap = config['Max_Memory_Utilization']/task.memory
-    net_cap = config['Max_Network_Utilization']/task.network
-    return math.ceil(min(cpu_cap, mem_cap, net_cap))
+This section is for testing
+
+'''
+def task_can_be_processed(task, cluster_state, parallelization, execution_time): 
+    # check if there is enough nodes to process the task
+    available_space = sum([node[task.arrival_time:(task.arrival_time+ task.deadline)].count(0) for node in cluster_state])
+    required_space = math.ceil((task.num_tasks/parallelization) * execution_time)
+    return available_space >= required_space
+
+def process_task(task, cluster_state, parallelization, execution_time):
+    required_space = math.ceil((task.num_tasks/parallelization) * execution_time)
+    print(f'The required space is: {required_space} slots')
+    
+    # Use round robin to schedule the task
+    node_idx = 0
+    num_nodes = len(cluster_state)
+    while required_space > 0:
+        print(f'Processing node {node_idx}', 'the current node state is: ', cluster_state[node_idx])
+        node = cluster_state[node_idx].copy()
+        for i in range(task.arrival_time, task.arrival_time + task.deadline):
+            if node[i] == 0:
+                node[i] = 1
+                required_space -= 1
+                if required_space == 0:
+                    break
+        cluster_state[node_idx] = node
+        node_idx = (node_idx + 1) % num_nodes
+        if required_space == 0:
+            return cluster_state
+    return cluster_state
+
+
+
+tast_task = data[0]
+default_cluster = [0] * 500
+tast_cluster_state = [default_cluster for _ in range(2)]
+
+print(task_can_be_processed(tast_task, tast_cluster_state, 2, 5))
+#print(process_task(tast_task, tast_cluster_state, 2, 5))
+
+output = process_task(tast_task, tast_cluster_state, 2, 5)
+print([node.count(1) for node in output])
+# %%
--- cluster-simulator/utils.py
+++ cluster-simulator/utils.py
@@ -63,3 +63,25 @@ def random_forest(data, node_lst):
     print(f"R^2 Score: {r2}")
 
     return best_model
+
+def get_min_nodes_lst(data, config):
+    lst = []
+    for workload in data:
+        lst.append(calc_min_nodes(workload, config))
+    return lst
+
+# find the minimum number of nodes needed to complete the workload
+def calc_min_nodes(task, config):
+    # load initial variables
+    node_count = 1
+    parallelization = get_num_task_per_node(task, config)
+    execution_time = config['Time_To_Complete_Task']
+    while (task.num_tasks * execution_time)/(node_count * parallelization) > task.deadline:
+        node_count += 1
+    return node_count
+
+def get_num_task_per_node(task, config):
+    cpu_cap = config['Max_CPU_Utilization']/task.cpu
+    mem_cap = config['Max_Memory_Utilization']/task.memory
+    net_cap = config['Max_Network_Utilization']/task.network
+    return math.ceil(min(cpu_cap, mem_cap, net_cap))

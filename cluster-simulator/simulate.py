6765e5e2edc13827d89c146806370e2d47c08568
Bowen Hou <bowen.hou@autodesk.com>
2024-01-23 23:58:35 -0500

    scale function complete

--- cluster-simulator/simulate.py
+++ cluster-simulator/simulate.py
@@ -1,10 +1,12 @@
 # %%
 from utils import load_workload, load_config, get_min_nodes_lst, get_num_task_per_node
 import math
+import logging
 
 data = load_workload('workload3')
 config = load_config()
 
+logging.getLogger().setLevel(logging.INFO)
 # %%
 # This function is used to simulate the cluster scaling process
 def scale_workload(data, config):
@@ -17,7 +19,7 @@ def scale_workload(data, config):
     rejected_task = 0
 
     # Initialize the cluster, where each node is empty for every second of the timeline
-    default_cluster = [0] * 500
+    default_cluster = [0] * max_time
     cluster_state = [default_cluster for _ in range(config['Initial_Node_Count'])]
 
     # load the constant time to complete a task
@@ -38,35 +40,146 @@ def scale_workload(data, config):
             task_queue.append(data.pop(0))
             parallization = get_num_task_per_node(task_queue, config)
     pass
-        
+
+
 def task_can_be_processed(task, cluster_state, parallelization, execution_time):
+    """
+    Checks if a task can be processed by the current cluster state considering the available space, parallelization level, and execution time.
+
+    Parameters:
+    - task: A Task object representing the task to be processed. The Task object should have attributes like num_tasks, arrival_time, and deadline.
+    - cluster_state: A list of lists representing the current state of the cluster. Each inner list represents a node and its elements represent the state of each slot in the node.
+    - parallelization: The level of parallelization for the task.
+    - execution_time: The execution time for the task.
+
+    Returns:
+    - A boolean value indicating whether the task can be processed. Returns True if there is enough space to process the task, and False otherwise.
+    """
     # check if there is enough nodes to process the task
     available_space = sum([node[task.arrival_time:task.deadline].count(0) for node in cluster_state])
     required_space = math.ceil(task.num_tasks/parallelization) * execution_time
     return available_space >= required_space
 
-# this is the task schedule function, which marks occupied space in the cluster as 1
 def process_task(task, cluster_state, parallelization, execution_time):
-    required_space = math.ceil((task.num_tasks/parallelization) * execution_time)
-    print(f'The required space is: {required_space} slots')
-    
-    # Use round robin to schedule the task
-    node_idx = 0
+    """
+    This function processes a task using a round-robin scheduling algorithm.
+
+    Parameters:
+    task: The task to be processed.
+    cluster_state: The current state of the cluster.
+    parallelization: The level of parallelization for the task.
+    execution_time: The execution time for the task.
+
+    Returns:
+    The updated state of the cluster after processing the task.
+    """
+
     num_nodes = len(cluster_state)
+    required_space = math.ceil((task.num_tasks/parallelization) * execution_time)
+    upper_bound = math.ceil(required_space/num_nodes) + task.arrival_time
+
+    logging.debug(f'The upper bound is: {upper_bound}')
+    logging.info(f'The required space is: {required_space} slots')
+
+    # Use round robin to schedule the task
+    current_node_idx = 0
+    logging.debug(f'The number of nodes is: {num_nodes}')
     while required_space > 0:
-        print(f'Processing node {node_idx}', 'the current node state is: ', cluster_state[node_idx])
-        node = cluster_state[node_idx].copy()
-        for i in range(task.arrival_time, task.arrival_time + task.deadline):
+        logging.debug(f'Processing node {current_node_idx}, the current node state is: {cluster_state[current_node_idx]}')
+        node = cluster_state[current_node_idx].copy()
+        for i in range(task.arrival_time, upper_bound):
             if node[i] == 0:
                 node[i] = 1
                 required_space -= 1
                 if required_space == 0:
                     break
-        cluster_state[node_idx] = node
-        node_idx = (node_idx + 1) % num_nodes
-        if required_space == 0:
-            return cluster_state
+        cluster_state[current_node_idx] = node
+        logging.debug(f'The node state after assignment is: {node}')
+        current_node_idx = (current_node_idx + 1) % num_nodes
+ 
+    logging.info(f'The cluster task count after assignment is: {[node.count(1) for node in cluster_state]}')
     return cluster_state
+
+def scale_down(cluster_state, target_node_count, timestamp):
+    """
+    Scales down the cluster by removing nodes and reassigning their tasks.
+
+    Parameters:
+    - cluster_state: A list of lists representing the current state of the cluster.
+    - target_node_count: The target number of nodes in the cluster after scaling down.
+    - timestamp: The current timestamp.
+
+    Returns:
+    - The updated state of the cluster after scaling down and reassigning tasks.
+    """
+    # concentrate all workload to the nodes that are not going to be removed
+    # calculate the total number of tasks scheduled in to be removed nodes after the timestamp
+    logging.info(f'********** Scaling down the cluster from {len(cluster_state)} to {target_node_count} nodes **********')
+    nodes_to_be_removed = cluster_state[target_node_count:]
+    total_tasks = sum([node[timestamp:].count(1) for node in nodes_to_be_removed])
+    logging.info(f'The total number of tasks to be reassigned is: {total_tasks}')
+    # calculate the number of tasks that should be assigned to each node
+    remaining_nodes = cluster_state[:target_node_count]
+
+    return reassign_task(remaining_nodes, total_tasks, timestamp)
+
+
+def scale_up(cluster_state, target_node_count, timestamp):
+    """
+    Scales up the cluster by adding new nodes.
+
+    Parameters:
+    - cluster_state: A list of lists representing the current state of the cluster. Each inner list represents a node and its elements represent the state of each slot in the node.
+    - target_node_count: The target number of nodes in the cluster after scaling up.
+
+    Returns:
+    - A new list of lists representing the state of the cluster after scaling up. The new nodes are added at the beginning of the list and are initialized with all slots being 0.
+    """
+    # add new nodes to the cluster
+    logging.info(f'********** Scaling up the cluster from {len(cluster_state)} to {target_node_count} nodes **********')
+    node_diff = target_node_count - len(cluster_state)
+    new_cluster = [[0] * len(cluster_state[0]) for _ in range(node_diff)] + cluster_state
+    total_tasks_to_assign = sum([node[timestamp:].count(1) for node in new_cluster])
+    logging.info(f'The total number of tasks to be reassigned is: {total_tasks_to_assign}')
+    return reassign_task(new_cluster, total_tasks_to_assign, timestamp)
+
+
+def reassign_task(cluster_state, total_tasks_to_assign, timestamp):
+    """
+    Reassigns tasks to nodes in the cluster.
+
+    Parameters:
+    - cluster_state: A list of lists representing the current state of the cluster.
+    - total_tasks_to_assign: The total number of tasks to be reassigned.
+    - timestamp: The current timestamp.
+
+    Returns:
+    - The updated state of the cluster after reassigning the tasks.
+    """
+    tasks_per_node = math.ceil(total_tasks_to_assign / len(cluster_state))
+    updated_cluster_state = cluster_state
+    logging.info(f'There are {total_tasks_to_assign} tasks unfinished and to be assigned to {len(cluster_state)} nodes, each node will be assigned {tasks_per_node} tasks')
+    current_node_idx = 0
+    while total_tasks_to_assign > 0:
+        node = updated_cluster_state[current_node_idx].copy()
+        try:
+            first_nonzero_index_after_timestamp = node[timestamp:].index(0) + timestamp
+        except ValueError:
+            logging.error(f"No available slots in node {current_node_idx} after timestamp {timestamp}")
+            break
+        logging.debug(f'Processing node {current_node_idx}, the current node state is: {cluster_state[current_node_idx]}')
+        for i in range(timestamp, first_nonzero_index_after_timestamp + tasks_per_node):
+            if node[i] == 0:
+                node[i] = 1
+                total_tasks_to_assign -= 1
+                if total_tasks_to_assign == 0:
+                    break
+        updated_cluster_state[current_node_idx] = node
+        logging.debug(f'The node state after assignment is: {node}')
+        current_node_idx = (current_node_idx + 1) % len(updated_cluster_state)
+    logging.info(f'The new cluster state after scaling is: {[node[timestamp:].count(1) for node in cluster_state]}')
+    return updated_cluster_state
+
 
 # %%
 '''
@@ -74,43 +187,22 @@ def process_task(task, cluster_state, parallelization, execution_time):
 This section is for testing
 
 '''
-def task_can_be_processed(task, cluster_state, parallelization, execution_time): 
-    # check if there is enough nodes to process the task
-    available_space = sum([node[task.arrival_time:(task.arrival_time+ task.deadline)].count(0) for node in cluster_state])
-    required_space = math.ceil((task.num_tasks/parallelization) * execution_time)
-    return available_space >= required_space
 
-def process_task(task, cluster_state, parallelization, execution_time):
-    required_space = math.ceil((task.num_tasks/parallelization) * execution_time)
-    print(f'The required space is: {required_space} slots')
-    
-    # Use round robin to schedule the task
-    node_idx = 0
-    num_nodes = len(cluster_state)
-    while required_space > 0:
-        print(f'Processing node {node_idx}', 'the current node state is: ', cluster_state[node_idx])
-        node = cluster_state[node_idx].copy()
-        for i in range(task.arrival_time, task.arrival_time + task.deadline):
-            if node[i] == 0:
-                node[i] = 1
-                required_space -= 1
-                if required_space == 0:
-                    break
-        cluster_state[node_idx] = node
-        node_idx = (node_idx + 1) % num_nodes
-        if required_space == 0:
-            return cluster_state
-    return cluster_state
+from utils import load_workload, load_config, get_min_nodes_lst, get_num_task_per_node
+import math
+import logging
 
+data = load_workload('workload3')
+config = load_config()
 
+logging.getLogger().setLevel(logging.INFO)
 
-tast_task = data[0]
-default_cluster = [0] * 500
-tast_cluster_state = [default_cluster for _ in range(2)]
+max_time = 30
 
-print(task_can_be_processed(tast_task, tast_cluster_state, 2, 5))
-#print(process_task(tast_task, tast_cluster_state, 2, 5))
+default_cluster = [0] * max_time
+cluster_state = [default_cluster for _ in range(5)]
+cluster_state = process_task(data[0], cluster_state, 1, 2)
 
-output = process_task(tast_task, tast_cluster_state, 2, 5)
-print([node.count(1) for node in output])
+logging.info(f'The cluster state is: {cluster_state}')
 # %%
+
--- cluster-simulator/utils.py
+++ cluster-simulator/utils.py
@@ -4,16 +4,45 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import GridSearchCV
+import math
+import logging
 
-# This helper function is used to print the utilization of the cluster
-def print_utilization(time, cpu_percent, mem_percent, net_percent, node_count, step=10):
-    cpu_string = ":" * int(cpu_percent//step) + " " * int(step - (cpu_percent)//step)
-    mem_string = ":" * int(mem_percent/step) + " " * int(step - (mem_percent)//step)
-    net_string = ":" * int(net_percent//step) + " " * int(step - (net_percent)//step)
+STEP_SIZE = 10
+
+logging.getLogger().setLevel(logging.INFO)
+
+def print_utilization(time, cpu_percent, mem_percent, net_percent, node_count, step=STEP_SIZE):
+    """
+    Prints the utilization of the cluster.
+
+    Parameters:
+    - time: The current time.
+    - cpu_percent: The CPU utilization percentage.
+    - mem_percent: The memory utilization percentage.
+    - net_percent: The network utilization percentage.
+    - node_count: The number of nodes.
+    - step: The step size for the utilization bar (default is 10).
+    """
+
+    cpu_string = ":" * (cpu_percent//step) + " " * (step - cpu_percent//step)
+    mem_string = ":" * (mem_percent//step) + " " * (step - mem_percent//step)
+    net_string = ":" * (net_percent//step) + " " * (step - net_percent//step)
 
     print(f"{time} | CPU: {cpu_string} {cpu_percent}% | Memory: {mem_string} {mem_percent}% | Network: {net_string} {net_percent}% | Nodes: {node_count}")
 
+
 def load_workload(fname):
+    """
+    This function loads a workload from a text file and returns a list of Task objects.
+
+    Parameters:
+    - fname: The name of the file (without extension) from which to load the workload.
+
+    Returns:
+    - A list of Task objects. Each Task object represents a single task in the workload.
+      The attributes of a Task object are loaded from the corresponding values in the file.
+      The file is expected to have six columns, which correspond to the six parameters of the Task constructor.
+    """
     with open(f'data/{fname}.txt', 'r') as f:
         line = [line.strip() for line in f.readlines()]
         # remove first line because it is the header
@@ -24,18 +53,38 @@ def load_workload(fname):
             data.append(Task(int(line[i][0]), int(line[i][1]), int(line[i][2]), int(line[i][3]), int(line[i][4]), int(line[i][5])))
     return data
 
-#load config json file
+
 def load_config():
+    """
+    Loads the configuration settings from a JSON file.
+
+    The JSON file is expected to be located in the 'data' directory and named 'config.json'.
+
+    Returns:
+    - A dictionary containing the configuration settings. The keys are the setting names and the values are the setting values.
+    """
     with open('data/config.json', 'r') as f:
         config = json.load(f)
     return config
 
+
 def random_forest(data, node_lst):
+    """
+    This function trains a RandomForestRegressor model using the given data and node list. 
+    It also performs grid search to find the best hyperparameters for the model.
+
+    Parameters:
+    - data: A list of Task objects. Each Task object represents a task with attributes like arrival_time, num_tasks, cpu, memory, network, and deadline.
+    - node_lst: A list of node counts associated with each task in the data.
+
+    Returns:
+    - The best RandomForestRegressor model trained with the optimal hyperparameters found during grid search.
+    """
+
     X = [[task.arrival_time, task.num_tasks, task.cpu, task.memory, task.network, task.deadline] for task in data]
     Y = node_lst
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
-    # %%
     model = RandomForestRegressor(n_estimators=100, random_state=42)
 
     # Train the model
@@ -58,20 +107,41 @@ def random_forest(data, node_lst):
     mse = mean_squared_error(Y_test, Y_pred)
     r2 = r2_score(Y_test, Y_pred)
 
-    print(f"Best parameters: {grid_search.best_params_}")
-    print(f"Mean Squared Error (MSE): {mse}")
-    print(f"R^2 Score: {r2}")
+    logging.info(f"Best parameters: {grid_search.best_params_}")
+    logging.info(f"Mean Squared Error (MSE): {mse}")
+    logging.info(f"R^2 Score: {r2}")
 
     return best_model
 
+
 def get_min_nodes_lst(data, config):
+    """
+    This function calculates the minimum number of nodes required for each task in the given data.
+
+    Parameters:
+    - data: A list of Task objects. Each Task object represents a task with attributes like arrival_time, num_tasks, cpu, memory, network, and deadline.
+    - config: A dictionary containing the configuration settings, such as 'Max_CPU_Utilization', 'Max_Memory_Utilization', and 'Max_Network_Utilization'.
+
+    Returns:
+    - A list of integers. Each integer represents the minimum number of nodes required for the corresponding task in the data.
+    """
     lst = []
     for workload in data:
         lst.append(calc_min_nodes(workload, config))
     return lst
 
-# find the minimum number of nodes needed to complete the workload
+
 def calc_min_nodes(task, config):
+    """
+    Calculates the minimum number of nodes required to complete a given task within its deadline.
+
+    Parameters:
+    - task: A Task object representing the task to be completed. The Task object should have attributes like num_tasks, cpu, memory, network, and deadline.
+    - config: A dictionary containing the configuration settings, such as 'Time_To_Complete_Task'.
+
+    Returns:
+    - An integer representing the minimum number of nodes required to complete the task within its deadline.
+    """
     # load initial variables
     node_count = 1
     parallelization = get_num_task_per_node(task, config)
@@ -80,7 +150,18 @@ def calc_min_nodes(task, config):
         node_count += 1
     return node_count
 
+
 def get_num_task_per_node(task, config):
+    """
+    Calculates the maximum number of tasks that can be run on a single node based on the resource capacities and task requirements.
+
+    Parameters:
+    - task: A Task object representing the task to be completed. The Task object should have attributes like cpu, memory, and network which represent the resources required by the task.
+    - config: A dictionary containing the configuration settings, such as 'Max_CPU_Utilization', 'Max_Memory_Utilization', and 'Max_Network_Utilization'.
+
+    Returns:
+    - An integer representing the maximum number of tasks that can be run on a single node.
+    """
     cpu_cap = config['Max_CPU_Utilization']/task.cpu
     mem_cap = config['Max_Memory_Utilization']/task.memory
     net_cap = config['Max_Network_Utilization']/task.network
